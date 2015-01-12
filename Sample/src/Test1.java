917fa3b0c69bf6b0f09a60b2710f6f3f40d363cc
sathishpearl@gmail.com <sathishpearl@gmail.com>
2015-01-12 13:46:38 +0530

    gfjfgj

--- Sample/src/Test1.java
+++ Sample/src/Test1.java
@@ -3,16 +3,23 @@ public class Test1 {
 	public void newMethod(){
 
 	}
+	public void new1Method(){
+
+	}
 
 	public static void main(String args[]){
 
+	}
+	
+	public void new2Method(){
+		
 	}
 	
 	public void new3Method(){
 		
 	}
 	
-	public void new1Method(){
+	public void new4Method(){
 		
 	}
 	
