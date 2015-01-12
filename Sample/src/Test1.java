3b7df0230fdd85086c1acdb3bde97af69597cca5
sathishpearl@gmail.com <sathishpearl@gmail.com>
2015-01-12 16:42:45 +0530

    sssss

--- Sample/src/Test1.java
+++ Sample/src/Test1.java
@@ -7,14 +7,20 @@ public class Test1 {
 	public static void main(String args[]){
 
 	}
-	
+
 	public void new2Method(){
-		
+
 	}
-	
+
 	public void new5Method(){
-		
+
 	}
+
+	public void new6Method(){
+
+	}
+
+
 
 
 }
